30868ca3fa98ed29e87aba2ae5a55dc73e64baf9
pboon09 <pboonchanachai@gmail.com>
2026-01-29 12:04:36 +0800

    Update robot_state_publisher.launch.py

--- src/turtle_description/launch/robot_state_publisher.launch.py
+++ src/turtle_description/launch/robot_state_publisher.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            parameters=[{'robot_description': robot_description}],
+            parameters=[{'robot_description': robot_description, 'use_sim_time': True}],
             output='screen'
         ),
     ])
